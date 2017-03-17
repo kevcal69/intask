f00bcd6f97943b0cff2c57bb8a8816cd1869a114
kevcal69 <kevcal69@gmail.com>
2017-03-17 18:34:20 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path')
+var webpack = require('webpack')
+var Bundletracker = require('webpack-bundle-tracker')
+
+module.exports = {
+    context: __dirname,
+    entry: './assets/js/index',
+    output: {
+        path: path.resolve('./assets/bundles/'),
+        filename: '[name]-[hash].js'
+    },
+    plugins: [
+        new Bundletracker({
+            filename: './webpack-stats.json'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        })
+    ],
+
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react']
+            }
+        }]
+    },
+
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.jsx']
+    }
+}
