174ad4cc18cd34430b61089ab9e21e597b43dda9
kevcal69 <kevcal69@gmail.com>
2017-03-17 19:09:02 +0800

    added new app task

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webpack_loader'
+    'webpack_loader',
+    'intask'
 ]
 
 MIDDLEWARE = [
